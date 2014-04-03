b2036830b30430b5a0491d6459e6e1f02f5982e4
vm <vm@b36ee4d2-03ae-4bd2-923c-95b5c71a064e>
2014-04-03 05:52:35 +0000

    updated bootstrap helper

--- trunk/comp/helpers/bootstrap_table/.php
+++ trunk/comp/helpers/bootstrap_table/.php
@@ -10,29 +10,38 @@ class cBootstrapTableHelper
       ($aIsChecked ? 'checked="checked" ' : '').'>';
   }
 
-  public static function gridBuild($aRecordset, $aParams)//!! grid -> table
+  public static function gridBuild($aRecordset, $aParams, $aFieldNames = array(),
+    $aFieldSizes = array())//!! grid -> table
   {
-    if (count($aRecordset) == 0)
+    if (!count($aRecordset) && !count($aFieldNames))
       return '';
 
-    return self::gridBuildByRows(self::recordsetToRows($aRecordset),
-      self::recordsetFieldNamesGet($aRecordset, $aParams));
+    return self::gridBuildByRows(
+      self::recordsetToRows($aRecordset),
+      (count($aFieldNames) ? $aFieldNames
+        : self::recordsetFieldNamesGet($aRecordset, $aParams)),
+      $aFieldSizes
+    );
   }
 
-  public static function gridBuildByRows($aRows, $aFieldNames)
+  public static function gridBuildByRows($aRows, $aFieldNames,
+    $aFieldSizes = array())
   {
     return self::TABLE_HEADER.
-      self::gridHeaderBuild($aFieldNames).
+      self::gridHeaderBuild($aFieldNames, $aFieldSizes).
       self::gridRowsBuild($aRows).
       self::TABLE_FOTER;
   }
 
-  public static function gridHeaderBuild($aFieldNames)
+  public static function gridHeaderBuild($aFieldNames, $aFieldSizes)
   {
     $lResult = '';
+    $lIsAddSize = count($aFieldSizes) > 0;
 
     for ($i = 0, $l = count($aFieldNames); $i < $l; $i++)
-      $lResult .= '<td><strong>'.$aFieldNames[$i].'</strong></td>';
+      $lResult .= '<td'.
+        ($lIsAddSize && $aFieldSizes[$i] ? ' width="'.$aFieldSizes[$i].'"' : '').
+        '><strong>'.$aFieldNames[$i].'</strong></td>';
 
     return '<thead><tr>'.$lResult.'</tr></thead>';
   }
@@ -40,6 +49,26 @@ class cBootstrapTableHelper
   public static function gridRowsBuild($aRows)
   {
     return '<tbody>'.implode('', $aRows).'</tbody>';
+  }
+
+  public static function recordsetAddEditButtons(&$aRecordset, $aParams,
+    $aUrlParams, $IsAddDeleteButton)
+  {
+    $lCallbackUrl = urlencode($aParams['ViewPageUrl'].'?'.$aUrlParams);
+
+    for ($i = 0, $l = count($aRecordset); $i < $l; $i++)
+    {
+      $aRecordset[$i]['edit'] =
+        '<a class="btn btn-success btn-sm" href="'.
+          $aParams['EditPageUrl'].'?'.$aRecordset[$i]['key_params'].
+          '&callback_url='.$lCallbackUrl.'">Edit</a>';
+      if ($IsAddDeleteButton)
+        $aRecordset[$i]['delete'] =
+          '<a class="btn btn-danger btn-sm" href="'.
+            $aParams['EditPageUrl'].'?edit_mode=delete&'.
+            $aRecordset[$i]['key_params'].'&callback_url='.$lCallbackUrl.
+            '">Delete</a>';
+    }
   }
 
   public static function recordsetFieldNamesGet($aRecordset, $aParams)
