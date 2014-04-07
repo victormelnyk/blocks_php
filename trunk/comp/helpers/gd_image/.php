06657419ddc8591b710760bb3a76b6d84189ab05
vm <vm@b36ee4d2-03ae-4bd2-923c-95b5c71a064e>
2014-04-07 04:55:53 +0000

    added gd image helper

---
+++ trunk/comp/helpers/gd_image/.php
@@ -0,0 +1,30 @@
+<?php
+class cGdImageHelper
+{
+  public static function resize($aSrcFlp, $ADestFlp, $aDestWidth, $aDestHeight)
+  {
+    $lTypes = array('', 'gif', 'jpeg', 'png');
+    list($lSrcWidth, $lSrcHeight, $lType) = getimagesize($aSrcFlp);
+    $lTypeS = $lTypes[$lType];
+    if (!$lTypeS)
+      throw new Exception('Not valid image type: "'.$lTypeS.'" for image: "'.
+        $aSrcFlp.'"');
+
+    $lXRatio = $aDestWidth / $lSrcWidth;
+    $lYRatio = $aDestHeight / $lSrcHeight;
+    $lRatio = min($lXRatio, $lYRatio);
+    $lIsUseXRatio = ($lXRatio == $lRatio);
+
+    $aDestWidth  = $lIsUseXRatio  ? $aDestWidth  : floor($lSrcWidth * $lRatio);
+    $aDestHeight = !$lIsUseXRatio ? $aDestHeight : floor($lSrcHeight * $lRatio);
+
+    $lCreateFunc = 'imagecreatefrom'.$lTypeS;
+    $lSrcImage = $lCreateFunc($aSrcFlp);
+    $lDestImage = imagecreatetruecolor($aDestWidth, $aDestHeight);
+    imagecopyresampled($lDestImage, $lSrcImage, 0, 0, 0, 0, $aDestWidth,
+      $aDestHeight, $lSrcWidth, $lSrcHeight);
+    $lSaveFunc = 'image'.$lTypeS;
+    return $lSaveFunc($lDestImage, $ADestFlp);
+  }
+}
+?>
